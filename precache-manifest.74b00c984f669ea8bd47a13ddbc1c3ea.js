952391000378cd5db66a79d1877d34736dca0b4c
akrigline <a.krigline@gmail.com>
2020-04-17 23:03:48 -0400

    Updates

---
+++ precache-manifest.74b00c984f669ea8bd47a13ddbc1c3ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd62756146c729f22e109f2cc03478be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c067c551e7c1e2c050a5",
+    "url": "/static/css/main.f5c30993.chunk.css"
+  },
+  {
+    "revision": "36df4558561219721a38",
+    "url": "/static/js/2.f4c49443.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4c49443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c067c551e7c1e2c050a5",
+    "url": "/static/js/main.cdb432d9.chunk.js"
+  },
+  {
+    "revision": "50c9ebaa664e2f169132",
+    "url": "/static/js/runtime-main.74154329.js"
+  },
+  {
+    "revision": "fe0586d51c20bdbcdc4458fad47bbd93",
+    "url": "/static/media/baseline-launch-24px.fe0586d5.svg"
+  },
+  {
+    "revision": "743a1e33ec05995eb78ebd391f79665c",
+    "url": "/static/media/logo.743a1e33.svg"
+  }
+]);
